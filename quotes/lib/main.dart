8cb63959b839faea9f21e3e4aa6be1941dddfb7c
jmkitavi <kitavimjoseph@gmail.com>
2020-05-06 08:40:01 +0300

    add quotes app

---
+++ quotes/lib/main.dart
@@ -0,0 +1,37 @@
+// Flutter
+import 'package:flutter/material.dart';
+import 'quote.dart';
+
+void main() => runApp(MaterialApp(
+  home: QuoteList(),
+));
+
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+
+  List<Quote> quotes = [
+    Quote(author: 'Oscar Wilde', text: 'Be yourself; everyone eles is already taken.'),
+    Quote(author: 'Oscar Wilde', text: 'I have nothing to declare except my genius.'),
+    Quote(author: 'Oscar Wilde', text: 'The truth is rarely pure and never simple.'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
+      ),
+    );
+  }
+}
